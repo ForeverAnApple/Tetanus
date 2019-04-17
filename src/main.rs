ebd5e1b8303f2c9d976a379dd72466ab105f5b67
ForeverAnApple <kinbd8@gmail.com>
2019-04-17 10:59:33 -0500

    Updated skeleton to handle large files and added error handling.

--- src/main.rs
+++ src/main.rs
@@ -24,23 +24,17 @@ use std::env;
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
 
-    match args.len() {
-        1 => {
-            println!("Usage: cargo run <rsa-file>");
-            return Ok(());
-        },
-        _ => { }
-    };
+    if args.len() != 2 {
+        println!("Usage: cargo run <rsa-file>");
+        return Ok(());
+    }
 
+    // the ? syntax is like a try catch loop, it's similar to the rust macro try!()
+    // Using a BufReader in case of very large files
     let file = File::open(&args[1])?;
     let buf = BufReader::new(file);
     let mut input_keys: Vec<String> = buf.lines().map(|l| l.unwrap()).collect();
     println!("\nLoaded {} keys from {}.", input_keys.len(), &args[1]);
-    /*
-    for key in input_keys{
-        println!("{}", key);
-    }
-    */
 
     analyze(&input_keys);
     
