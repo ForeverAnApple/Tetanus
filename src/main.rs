a5463d5599eeba55d8e881a3285a4f9971c40ca8
ForeverAnApple <kinbd8@gmail.com>
2019-04-17 22:52:15 -0500

    Added gmp functionality from cargo package rug (1.3.0). Added parsing ability to parse input hex data into rug::Integer. Added small testcase to test Batch-GCD function.

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,13 @@
  *
  * Daiwei Chen, Cole Houston
  * 2019-04-16
+ * Check LICENSE for licensing information.
  */
 
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::env;
-
-//fn analyze(keys: Vec<String>);
+use rug::{Assign, Integer}; // big numbers
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
@@ -29,18 +29,45 @@ fn main() -> Result<(), io::Error> {
         return Ok(());
     }
 
-    // the ? syntax is like a try catch loop, it's similar to the rust macro try!()
-    // Using a BufReader in case of very large files
-    let file = File::open(&args[1])?;
-    let buf = BufReader::new(file);
-    let mut input_keys: Vec<String> = buf.lines().map(|l| l.unwrap()).collect();
-    println!("\nLoaded {} keys from {}.", input_keys.len(), &args[1]);
-
-    analyze(&input_keys);
+    let mut input_keys: Vec<String> = Vec::new();
+    
+    match args[1].as_ref() {
+        "test" => {
+            input_keys =
+                vec!["775".into(), "b6b".into(), "123".into(), "cd".into(), "3dd".into(),
+                     "3e".into(), "1c3".into(), "797".into(), "437".into(), "973".into()];
+            println!("Testing keys: {:?}", input_keys);
+        }
+        _ => {
+            // the ? syntax is like a try catch loop, it's similar to the rust macro try!()
+            // Using a BufReader in case of very large files
+            let file = File::open(&args[1])?;
+            let buf = BufReader::new(file);
+            input_keys = buf.lines().map(|l| l.unwrap()).collect();
+            println!("\nLoaded {} keys from {}.", input_keys.len(), &args[1]);
+        }
+    }
+    
+    
+    // Load all the hex keys into rug
+    let mut rug_keys: Vec<Integer> = Vec::new();
+    for (i, key) in input_keys.iter().enumerate(){
+        let mut parsed = Integer::new();
+        parsed.assign(Integer::parse_radix(key, 16).unwrap());
+        println!("Parsing {} into {:?}", key, &parsed);
+        rug_keys.push(parsed);
+    }
+    
+    analyze(&rug_keys);
     
     Ok(())
 }
 
-fn analyze(keys: &Vec<String>) {
+fn analyze(keys: &Vec<Integer>) {
     println!("Starting analysis on {} keys...", keys.len());
+    
+}
+
+fn product() {
+
 }
