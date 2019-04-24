eaa862d12db21c878f0fc82d0be60b7448fd5ba5
ForeverAnApple <kinbd8@gmail.com>
2019-04-24 10:59:20 -0500

    Updated version and formatting.

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,8 @@ fn main() -> Result<(), io::Error> {
 	      }
 	      "keyTest" => {
 	          recreate_rsa("1090660992520643446103273789680343", "1162435056374824133712043309728653", "65537");
-                  recreate_rsa("13188561715320670649980058508476937575420932981817182290137079231671393627992273609428765926756863372112632409435820759987478412106280802494610337777961169", "9869736251765338993559973095861800023798699503010698782849009018533778193969885656418441889800662183510264452340003785864584329793753382298493793447912119", "65537");
+            println!("=====================");
+            recreate_rsa("13188561715320670649980058508476937575420932981817182290137079231671393627992273609428765926756863372112632409435820759987478412106280802494610337777961169", "9869736251765338993559973095861800023798699503010698782849009018533778193969885656418441889800662183510264452340003785864584329793753382298493793447912119", "65537");
 	          return Ok(());
 	      }
         _ => {
@@ -218,11 +219,11 @@ fn remainder_tree(prod_tree: &Vec<Vec<Integer>>, keys: &Vec<Integer>) -> Vec<Int
     }
     /*
     for key in keys {
-        let mut modu = Integer::new();
-        modu.assign(&rootnum % key);
-        rems.push(modu);
-    }
-    */
+    let mut modu = Integer::new();
+    modu.assign(&rootnum % key);
+    rems.push(modu);
+}
+     */
     rems
 }
 
@@ -244,36 +245,36 @@ fn recreate_rsa(mut stringP:&str, mut stringQ:&str, mut encryption:&str) {
     println!("[private_key]");
     println!("version=INTEGER:0");
 
-	let mut n = Integer::new();
-	let mut p = Integer::new();
-	let mut q = Integer::new();
-	let mut e = Integer::new();
+	  let mut n = Integer::new();
+	  let mut p = Integer::new();
+	  let mut q = Integer::new();
+	  let mut e = Integer::new();
 
-	n.assign(Integer::parse(stringP).unwrap());
-	p.assign(Integer::parse(stringP).unwrap());
-	q.assign(Integer::parse(stringQ).unwrap());
-	e.assign(Integer::parse(encryption).unwrap());
+	  n.assign(Integer::parse(stringP).unwrap());
+	  p.assign(Integer::parse(stringP).unwrap());
+	  q.assign(Integer::parse(stringQ).unwrap());
+	  e.assign(Integer::parse(encryption).unwrap());
 
   	let mut phi=Integer::from(1);
   	let mut p2=Integer::from(1);
   	let mut q2=Integer::from(1);
 
   	n.mul_from(&q);
- 	e.add_from(0);
+ 	  e.add_from(0);
 
- 	println!("n=INTEGER:0x{:x}", n);
+ 	  println!("n=INTEGER:0x{:x}", n);
   	println!("e=INTEGER:0x{:x}", e);
 
   	phi.sub_from(&p);
   	p2.sub_from(&p);
   	q2.sub_from(&q);
- 	phi.mul_from(&q2);
-	let mut d=Integer::from(e);
-	d.invert_mut(&phi);
+ 	  phi.mul_from(&q2);
+	  let mut d=Integer::from(e);
+	  d.invert_mut(&phi);
 
-	println!("d=INTEGER:0x{:x}", d);
+	  println!("d=INTEGER:0x{:x}", d);
 
-	println!("p=INTEGER:0x{:x}", p);
+	  println!("p=INTEGER:0x{:x}", p);
   	println!("q=INTEGER:0x{:x}", q);
   	
   	p2.rem_from(&d);
