6a91adb60385f91abf4dbe7c35b5fd28b823ef9e
ackin <colehouston44@gmail.com>
2019-04-24 08:19:56 -0500

    bash script to reconstruct key from rust program's output

--- aux_tools/extractE.py
+++
@@ -1,6 +0,0 @@
-import sys
-from Crypto.PublicKey import RSA
-
-key=RSA.importKey(open(sys.argv[1], 'r').read())
-e=hex(key.e)
-print("The E portion of your public key is: 0x" + format(key.e, 'x'))
---
+++ aux_tools/keyRegen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: bash $0 <input.key> <output>"
+    exit
+fi;
+
+openssl asn1parse -genconf $1 -out newkey.der
+openssl rsa -in newkey.der -inform der -out $2
+rm newkey.der
+openssl rsa -in $2 -check -text
